a977a6096c592e044797e787557683946a7359f6
abhinavmabhi <dexmarketing192018@gmail.com>
2025-05-05 13:09:10 +0530

    first commit

---
+++ Earth_nd_Scale/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Earth_nd_Scale project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Earth_nd_Scale.settings')
+
+application = get_asgi_application()
---
+++ Earth_nd_Scale/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Earth_nd_Scale project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Earth_nd_Scale.settings')
+
+application = get_wsgi_application()
